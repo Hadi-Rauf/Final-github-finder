b25d145774aeb9be56aa1e323b5efeee7a47bb13
hadimohammad <69950917+hadimohammad@users.noreply.github.com>
2021-10-26 07:49:46 +0000

    -

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About the GitHub Finder App</h1>
+            <p>The purpose of this app is to allow users to search GitHub Users</p>
+            <h6>Made by Hadi</h6>
+        </Fragment>
+    )
+}
+
+export default About
